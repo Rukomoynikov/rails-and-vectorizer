3d502b747acd6dc37b5635f9bbc280c18faaade3
Max Rukomoynikov <max@apply4.com>
2022-08-26 12:06:46 +0400

    LDB-1: Poem about bears

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    this.element.textContent = "Raul, i want to drink some beer! Ok, bears usually aren't happy when i try to drink them."
   }
 }
